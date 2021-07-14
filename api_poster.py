c7692fa2baa8c14c0fa3846c1cdcd5065ec84417
muhammad Gulbaz <mgulbazilyas@gmail.com>
2021-07-14 17:25:49 +0500

    update api_poster.py

--- api_poster.py
+++ api_poster.py
@@ -16,16 +16,16 @@ class GUKLYAPI:
         return self.session.get(self.endpoint, params=params,).json()
     
     def get(self, pk):
-        return self.session.get(self.endpoint + pk + '/', ).json()
+        return self.session.get(self.endpoint + str(pk) + '/', ).json()
     
     def create(self, data):
         return self.session.post(self.endpoint, data)
 
     def delete(self, pk):
-        return self.session.delete(self.endpoint + pk + '/', ).json()
+        return self.session.delete(self.endpoint + str(pk) + '/', ).json()
     
     def update(self, pk, data):
-        return self.session.patch(self.endpoint + pk + '/', data=data).json()
+        return self.session.patch(self.endpoint + str(pk) + '/', data=data).json()
 
 
 api = GUKLYAPI(API_TOKEN, 'https://gukly.com/clients/lionpost/')
